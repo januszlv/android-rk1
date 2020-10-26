a5155ec77c5c15658ed8446da217be92f9fabdc1
Darovskaya Anastasia <stiltskenx@gmail.com>
2020-10-26 16:21:27 +0000

    data format changed [DarovskayaAI]

--- app/src/main/java/com/example/rk1/DayPriceAdapter.kt
+++ app/src/main/java/com/example/rk1/DayPriceAdapter.kt
@@ -6,6 +6,7 @@ import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.rk1.databinding.DayPriceItemBinding
+import java.text.SimpleDateFormat
 import java.util.*
 
 class DayPriceAdapter(
@@ -16,7 +17,7 @@ class DayPriceAdapter(
         RecyclerView.ViewHolder(binding.root) {
 
         fun bind(item: DayPriceViewModel.DayPriceItemModel) {
-            binding.date = Date(item.ts * 1000).toString()
+            binding.date = Date(item.ts * 1000).toString("dd MMM yyyy")
             binding.sym = item.sym
             binding.value = item.value.toString()
         }
@@ -29,5 +30,10 @@ class DayPriceAdapter(
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bind(dayPrices[position])
+    }
+
+    private fun Date.toString(format: String, locale: Locale = Locale.getDefault()): String {
+        val formatter = SimpleDateFormat(format, locale)
+        return formatter.format(this)
     }
 }
