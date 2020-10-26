76a1932b0dd5761b117ff4f5af475782bb8264dd
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2020-10-26 12:44:05 +0300

    menu

--- app/src/main/java/com/example/rk1/MainActivity.kt
+++ app/src/main/java/com/example/rk1/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.rk1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_update -> {
+                true
+            }
+            R.id.menu_settings -> {
+                true
+            }
+            else -> {
+                super.onOptionsItemSelected(item)
+            }
+        }
     }
 }
